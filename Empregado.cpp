055ac64ed3a21be58ae166921d725e41c94c71c9
FranciscoNevesT <francisconevest@outlook.com>
2020-10-15 14:21:04 -0300

    Add Empregado.cpp

---
+++ Empregado.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Empregado.hpp"
+
+Empregado::pagamentoMes(double horasTrabalhadas){     
+      double t = horasTrabalhadas;
+	  
+	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
+      if (horasTrabalhadas > 8) {
+        double x = horasTrabalhadas - 8;
+        t += x / 2;
+      }
+	  return t * salarioHora;
+    }
